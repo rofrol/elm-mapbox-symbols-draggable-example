9d3051fea710f17119be59791e2b251376b427e0
Roman Frołow <rofrol@gmail.com>
2019-02-12 23:20:39 +0100

    Prepare for style generated for style-generator

--- src/Main.elm
+++ src/Main.elm
@@ -4,7 +4,7 @@ module Main exposing (main)
 
 import Browser
 import Html exposing (div, text)
-import Html.Attributes exposing (style)
+import Html.Attributes as Attrs
 import Json.Decode
 import Json.Encode
 import LngLat exposing (LngLat)
@@ -83,7 +83,7 @@ view model =
     { title = "Mapbox Example"
     , body =
         [ css
-        , div [ style "width" "100vw", style "height" "100vh" ]
+        , div [ Attrs.style "width" "100vw", Attrs.style "height" "100vh" ]
             [ map
                 [ maxZoom 24
                 , onMouseMove Hover
@@ -92,67 +92,315 @@ view model =
                 , eventFeaturesLayers [ "changes" ]
                 , hoveredFeatures model.features
                 ]
-                (Style
-                    { transition = Style.defaultTransition
-                    , light = Style.defaultLight
-                    , sources =
-                        [ Source.vectorFromUrl "composite" "mapbox://mapbox.mapbox-terrain-v2,mapbox.mapbox-streets-v7"
-                        , Source.geoJSONFromValue "changes" [] geojson
-                        ]
-                    , misc =
-                        [ Style.name "light"
-                        , Style.defaultCenter <| LngLat 20.39789404164037 43.22523201923144
-                        , Style.defaultZoomLevel 1.5967483759772743
-                        , Style.sprite "mapbox://sprites/mapbox/streets-v7"
-                        , Style.glyphs "mapbox://fonts/mapbox/{fontstack}/{range}.pbf"
-                        ]
-                    , layers =
-                        [ Layer.background "background"
-                            [ E.rgba 246 246 244 1 |> Layer.backgroundColor
-                            ]
-                        , Layer.fill "landcover"
-                            "composite"
-                            [ Layer.sourceLayer "landcover"
-                            , E.any
-                                [ E.getProperty (str "class") |> E.isEqual (str "wood")
-                                , E.getProperty (str "class") |> E.isEqual (str "scrub")
-                                , E.getProperty (str "class") |> E.isEqual (str "grass")
-                                , E.getProperty (str "class") |> E.isEqual (str "crop")
-                                ]
-                                |> Layer.filter
-                            , Layer.fillColor (E.rgba 227 227 227 1)
-                            , Layer.fillOpacity (float 0.6)
-                            ]
-                        , Layer.symbol "place-city-lg-n"
-                            "composite"
-                            [ Layer.sourceLayer "place_label"
-                            , Layer.minzoom 1
-                            , Layer.maxzoom 24
-                            , Layer.filter <|
-                                E.all
-                                    [ E.getProperty (str "scalerank") |> E.greaterThan (int 2)
-                                    , E.getProperty (str "type") |> E.isEqual (str "city")
-                                    ]
-                            , Layer.textField <|
-                                E.format
-                                    [ E.getProperty (str "name_en")
-                                        |> E.formatted
-                                        |> E.fontScaledBy (float 1.2)
-                                    , E.formatted (str "\n")
-                                    , E.getProperty (str "name")
-                                        |> E.formatted
-                                        |> E.fontScaledBy (float 0.8)
-                                        |> E.withFont (E.strings [ "DIN Offc Pro Medium" ])
-                                    ]
-                            ]
-                        , Layer.fill "changes"
-                            "changes"
-                            [ Layer.fillOpacity (E.ifElse (E.toBool (E.featureState (str "hover"))) (float 0.9) (float 0.1))
-                            ]
-                        ]
-                    }
-                )
-            , div [ style "position" "absolute", style "bottom" "20px", style "left" "20px" ] [ text (LngLat.toString model.position) ]
+                style2
+            , div [ Attrs.style "position" "absolute", Attrs.style "bottom" "20px", Attrs.style "left" "20px" ] [ text (LngLat.toString model.position) ]
             ]
         ]
     }
+
+
+style2 =
+    Style
+        { transition = Style.defaultTransition
+        , light = Style.defaultLight
+        , sources =
+            [ Source.vectorFromUrl "composite" "mapbox://mapbox.mapbox-terrain-v2,mapbox.mapbox-streets-v7"
+            , Source.geoJSONFromValue "changes" [] geojson2
+            ]
+        , misc =
+            [ Style.name "light"
+            , Style.defaultCenter <| LngLat -77.034084 38.909671
+            , Style.defaultZoomLevel 14
+            , Style.sprite "mapbox://sprites/mapbox/light-v10"
+            , Style.glyphs "mapbox://fonts/mapbox/{fontstack}/{range}.pbf"
+            ]
+        , layers =
+            [ Layer.background "background"
+                [ E.rgba 246 246 244 1 |> Layer.backgroundColor
+                ]
+            , Layer.fill "landcover"
+                "composite"
+                [ Layer.sourceLayer "landcover"
+                , E.any
+                    [ E.getProperty (str "class") |> E.isEqual (str "wood")
+                    , E.getProperty (str "class") |> E.isEqual (str "scrub")
+                    , E.getProperty (str "class") |> E.isEqual (str "grass")
+                    , E.getProperty (str "class") |> E.isEqual (str "crop")
+                    ]
+                    |> Layer.filter
+                , Layer.fillColor (E.rgba 227 227 227 1)
+                , Layer.fillOpacity (float 0.6)
+                ]
+            , Layer.symbol "place-city-lg-n"
+                "composite"
+                [ Layer.sourceLayer "place_label"
+                , Layer.minzoom 1
+                , Layer.maxzoom 24
+                , Layer.filter <|
+                    E.all
+                        [ E.getProperty (str "scalerank") |> E.greaterThan (int 2)
+                        , E.getProperty (str "type") |> E.isEqual (str "city")
+                        ]
+                , Layer.textField <|
+                    E.format
+                        [ E.getProperty (str "name_en")
+                            |> E.formatted
+                            |> E.fontScaledBy (float 1.2)
+                        , E.formatted (str "\n")
+                        , E.getProperty (str "name")
+                            |> E.formatted
+                            |> E.fontScaledBy (float 0.8)
+                            |> E.withFont (E.strings [ "DIN Offc Pro Medium" ])
+                        ]
+                ]
+            , Layer.fill "changes"
+                "changes"
+                [ Layer.fillOpacity (E.ifElse (E.toBool (E.featureState (str "hover"))) (float 0.9) (float 0.1))
+                ]
+            ]
+        }
+
+
+geojson2 =
+    Json.Decode.decodeString Json.Decode.value """
+{
+  "type": "FeatureCollection",
+  "features": [
+    {
+      "type": "Feature",
+      "geometry": {
+        "type": "Point",
+        "coordinates": [
+          -77.034084142948,
+          38.909671288923
+        ]
+      },
+      "properties": {
+        "phoneFormatted": "(202) 234-7336",
+        "phone": "2022347336",
+        "address": "1471 P St NW",
+        "city": "Washington DC",
+        "country": "United States",
+        "crossStreet": "at 15th St NW",
+        "postalCode": "20005",
+        "state": "D.C."
+      }
+    },
+    {
+      "type": "Feature",
+      "geometry": {
+        "type": "Point",
+        "coordinates": [
+          -77.049766,
+          38.900772
+        ]
+      },
+      "properties": {
+        "phoneFormatted": "(202) 507-8357",
+        "phone": "2025078357",
+        "address": "2221 I St NW",
+        "city": "Washington DC",
+        "country": "United States",
+        "crossStreet": "at 22nd St NW",
+        "postalCode": "20037",
+        "state": "D.C."
+      }
+    },
+    {
+      "type": "Feature",
+      "geometry": {
+        "type": "Point",
+        "coordinates": [
+          -77.043929,
+          38.910525
+        ]
+      },
+      "properties": {
+        "phoneFormatted": "(202) 387-9338",
+        "phone": "2023879338",
+        "address": "1512 Connecticut Ave NW",
+        "city": "Washington DC",
+        "country": "United States",
+        "crossStreet": "at Dupont Circle",
+        "postalCode": "20036",
+        "state": "D.C."
+      }
+    },
+    {
+      "type": "Feature",
+      "geometry": {
+        "type": "Point",
+        "coordinates": [
+          -77.0672,
+          38.90516896
+        ]
+      },
+      "properties": {
+        "phoneFormatted": "(202) 337-9338",
+        "phone": "2023379338",
+        "address": "3333 M St NW",
+        "city": "Washington DC",
+        "country": "United States",
+        "crossStreet": "at 34th St NW",
+        "postalCode": "20007",
+        "state": "D.C."
+      }
+    },
+    {
+      "type": "Feature",
+      "geometry": {
+        "type": "Point",
+        "coordinates": [
+          -77.002583742142,
+          38.887041080933
+        ]
+      },
+      "properties": {
+        "phoneFormatted": "(202) 547-9338",
+        "phone": "2025479338",
+        "address": "221 Pennsylvania Ave SE",
+        "city": "Washington DC",
+        "country": "United States",
+        "crossStreet": "btwn 2nd & 3rd Sts. SE",
+        "postalCode": "20003",
+        "state": "D.C."
+      }
+    },
+    {
+      "type": "Feature",
+      "geometry": {
+        "type": "Point",
+        "coordinates": [
+          -76.933492720127,
+          38.99225245786
+        ]
+      },
+      "properties": {
+        "address": "8204 Baltimore Ave",
+        "city": "College Park",
+        "country": "United States",
+        "postalCode": "20740",
+        "state": "MD"
+      }
+    },
+    {
+      "type": "Feature",
+      "geometry": {
+        "type": "Point",
+        "coordinates": [
+          -77.097083330154,
+          38.980979
+        ]
+      },
+      "properties": {
+        "phoneFormatted": "(301) 654-7336",
+        "phone": "3016547336",
+        "address": "4831 Bethesda Ave",
+        "cc": "US",
+        "city": "Bethesda",
+        "country": "United States",
+        "postalCode": "20814",
+        "state": "MD"
+      }
+    },
+    {
+      "type": "Feature",
+      "geometry": {
+        "type": "Point",
+        "coordinates": [
+          -77.359425054188,
+          38.958058116661
+        ]
+      },
+      "properties": {
+        "phoneFormatted": "(571) 203-0082",
+        "phone": "5712030082",
+        "address": "11935 Democracy Dr",
+        "city": "Reston",
+        "country": "United States",
+        "crossStreet": "btw Explorer & Library",
+        "postalCode": "20190",
+        "state": "VA"
+      }
+    },
+    {
+      "type": "Feature",
+      "geometry": {
+        "type": "Point",
+        "coordinates": [
+          -77.10853099823,
+          38.880100922392
+        ]
+      },
+      "properties": {
+        "phoneFormatted": "(703) 522-2016",
+        "phone": "7035222016",
+        "address": "4075 Wilson Blvd",
+        "city": "Arlington",
+        "country": "United States",
+        "crossStreet": "at N Randolph St.",
+        "postalCode": "22203",
+        "state": "VA"
+      }
+    },
+    {
+      "type": "Feature",
+      "geometry": {
+        "type": "Point",
+        "coordinates": [
+          -75.28784,
+          40.008008
+        ]
+      },
+      "properties": {
+        "phoneFormatted": "(610) 642-9400",
+        "phone": "6106429400",
+        "address": "68 Coulter Ave",
+        "city": "Ardmore",
+        "country": "United States",
+        "postalCode": "19003",
+        "state": "PA"
+      }
+    },
+    {
+      "type": "Feature",
+      "geometry": {
+        "type": "Point",
+        "coordinates": [
+          -75.20121216774,
+          39.954030175164
+        ]
+      },
+      "properties": {
+        "phoneFormatted": "(215) 386-1365",
+        "phone": "2153861365",
+        "address": "3925 Walnut St",
+        "city": "Philadelphia",
+        "country": "United States",
+        "postalCode": "19104",
+        "state": "PA"
+      }
+    },
+    {
+      "type": "Feature",
+      "geometry": {
+        "type": "Point",
+        "coordinates": [
+          -77.043959498405,
+          38.903883387232
+        ]
+      },
+      "properties": {
+        "phoneFormatted": "(202) 331-3355",
+        "phone": "2023313355",
+        "address": "1901 L St. NW",
+        "city": "Washington DC",
+        "country": "United States",
+        "crossStreet": "at 19th St",
+        "postalCode": "20036",
+        "state": "D.C."
+      }
+    }
+  ]
+}
+""" |> Result.withDefault (Json.Encode.object [])
